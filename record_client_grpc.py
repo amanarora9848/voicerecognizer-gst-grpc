ebeb4f9211121e6bff16dc8fed96e6162ffb5f8a
amanarora9848 <aman.arora9848@gmail.com>
2022-11-01 18:07:49 +0100

    Added requirements.txt

--- record_client_grpc.py
+++ record_client_grpc.py
@@ -5,7 +5,7 @@ import recognize_pb2_grpc
 def run():
     with grpc.insecure_channel('0.0.0.0:50052') as channel:
         stub = recognize_pb2_grpc.RecognizerServiceStub(channel)
-        result = stub.Recognize(recognize_pb2.Control(start = 0))
+        result = stub.Recognize(recognize_pb2.Control(start = 1))
         print(result)
 
 if __name__ == '__main__':
